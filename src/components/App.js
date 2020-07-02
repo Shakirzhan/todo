23a4420d972c54241663c8e43aabc5c8e8ec06a8
Shakirzhan <samarbaev99@mail.ru>
2020-07-02 09:01:42 +0300

    preparatory work

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { RouteNode } from 'react-router5';
 
@@ -10,7 +11,7 @@ import {
 
 import '../style/main.css';
 
-import styles from '../style/FriendListApp.css';
+import styles from '../style/style.css';
 
 class App extends Component {
 
@@ -21,6 +22,25 @@ class App extends Component {
                 <Route exact path="/">
                   <div className={styles.friendListApp}>
                     <h1>The FriendList</h1>
+                    <input
+                      type="text"
+                      autoFocus="true"
+                      className={classnames('form-control', styles.addFriendInput)}
+                      placeholder="Type the name of a friend"
+                    />
+                    <ul className={styles.friendList}>
+                      <li className={styles.friendListItem}>
+                        <div className={styles.friendInfos}>
+                          <div><span>Name</span></div>
+                          <div><small>xx friends in common</small></div>
+                        </div>
+                        <div className={styles.friendActions}>
+                          <button className={`btn btn-default ${styles.btnAction}`}>
+                            <i className="fa fa-trash" />
+                          </button>
+                        </div>
+                      </li>
+                    </ul>
                   </div>
                 </Route>
               </Switch>
