825a67f44697e8318c4621a850c0a6920f234ed7
Shakirzhan <samarbaev99@mail.ru>
2020-07-14 12:00:21 +0300

    server check

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://hack/api/"
+  baseURL: "http://shakirzhan.ru/api/"
 });
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     })],
     devServer: {
         historyApiFallback: true,
-        port: 80,
-        host: '0.0.0.0'
+        port: 3000,
+        host: 'localhost'
     }
 }
